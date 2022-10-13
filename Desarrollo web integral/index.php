98d4f46988c14e2029dd096fd29d45c32bab6341
JRosales724 <115046789+JRosales724@users.noreply.github.com>
2022-10-13 17:56:19 -0500

    Add files via upload

---
+++ Desarrollo web integral/index.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="imagenes/logo.png" />
+    <title>HP OMEN GAMING PC'S</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">HP <span>OMEN</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Inicio</a></li>
+                <li><a href="#about" class="menu-btn">Acerca de</a></li>
+            
+            <!-- <li><a href="#services" class="menu-btn">Servicios</a></li> -->
+               <li><a href="#galery" class="menu-btn">Galeria</a></li>
+               <li><a href="#distribucion" class="menu-btn">Distribución</a></li>
+               <li><a href="#teams" class="menu-btn">Creadores</a></li>
+               <li><a href="#contact" class="menu-btn">Contacto</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Ahora ya nada te detendrá.</div>
+                <div class="text-2">OMEN GAMING</div>
+                <div class="text-3">Lo mejor en  <span class="typing"></span></div>
+                <a href="https://www.facebook.com/HP/" target="_blank" rel="noopener noreferrer">Visita nuestra pagina de FB</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">Acerca de nosotros</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="imagenes/logo_omen.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">En HP somos <span class="typing-2"></span></div>
+                    <p>Somos una compañía de tecnología que nació bajo la creencia de que las empresas deben hacer más que simplemente obtener una ganancia. Deben hacer del mundo un lugar mejor, con más de 80 años de acciones que prueban nuestras intenciones, tenemos la confianza necesaria para imaginar un mundo donde la innovación impulse contribuciones extraordinarias para la humanidad..</p>
+                    <a href="https://www.hp.com/mx-es/hp-information.html" target="_blank" rel="noopener noreferrer">Contactanos</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <!--
+    <section class="services" id="services">
+        <div class="max-width">
+            <h2 class="title">Nuestros servicios</h2>
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-paint-brush"></i>
+                        <div class="text">Web Design</div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia sunt, quasi quo illo enim.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-chart-line"></i>
+                        <div class="text">Advertising</div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia sunt, quasi quo illo enim.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-code"></i>
+                        <div class="text">Apps Design</div>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem quia sunt, quasi quo illo enim.</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        
+    </section>
+    -->
+
+    <!-- skills section start -->
+    <!-- inabilitado por el momento
+    
+-->
+
+<!-- Galery section start -->
+    <section class="teams" id="galery">
+        <div class="max-width">
+            <h2 class="title">Conoce la gama</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="imagenes/cpu.jpg" alt="">
+                        <div class="text">CPU OMEN GAMING</div>
+                        <p>Expande tu mundo gaming.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="imagenes/omen16.png" alt="">
+                        <div class="text">OMEN 16</div>
+                        <p>Eleva tu juego a un nivel superior.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="imagenes/omen30l.png" alt="">
+                        <div class="text">HP OMEN 30L</div>
+                        <p>Diseño sin palabras donde el tiempo pasa volando.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="imagenes/teclado_audifonos.jpg" alt="">
+                        <div class="text">ACCESORIOS</div>
+                        <p>Complementa la experiencia gaming.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="imagenes/omen2.jpg" alt="">
+                        <div class="text">HP OMEN 15</div>
+                        <p>Laptop gaming omen 15 $6900.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+<!-- ################################################################################## -->
+<section class="skills" id="distribucion">
+        <div class="max-width">
+            <h2 class="title">¿Que productos vende HP?</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">Distribucion de productos HP.</div>
+                    <p>Somos una compañia con un catalago extenso de productos tal como lo es la venta de partes de hardware como memorias RAM, HDD asi como tambien, venta de impresoras, mouse, teclados, laptops. PC para uso domestico y Pc de uso Gamer.</p>
+                    <a href="https://www.omen.com/mx/es.html">Ve todos nuestros productos!</a>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>LAPTOP</span>
+                            <span>90%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>TECLADOS</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PC GAMER</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>IMPRESORAS</span>
+                            <span>50%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PIEZAS DE HARDWARE</span>
+                            <span>70%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  <!-- teams section start -->
+  
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">Desarrolladores</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="team/brandonx.jpg" alt="">
+                        <div class="text">Brandon Ramiro Hernandez Mendez</div>
+                        <p>Administrador de hardware.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="team/luisilloelpillo.jpg" alt="">
+                        <div class="text">Luis Angel Martinez Ramirez</div>
+                        <p>Diseñador de equipos de escritorio.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="team/jesusxd.jpg" alt="">
+                        <div class="text">Jesus Roberto Rosales Covarrubias</div>
+                        <p>Diseñador de equipos portables.</p>
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+    </section>
+    -->
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contactanos</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Ponerse en contacto</div>
+                    <p>HP es una empresa que distribuye a nivel mundial contando con puntos de venta asi como oficinas y centros de reparacion de equipos en distintas ciudades de la republica mexicana en ciudades como Monterrey, Guadalajara, CDMX etc. Esto con el fin de dar un mejor servicio a nuestros clientes y publico en general.</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Nombre</div>
+                                <div class="sub-title">Contact HP</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Dirección</div>
+                                <div class="sub-title">Calzada del Valle n 116 A. Monterrey, Nuevo León</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">atencionalclientehp@hp.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+                <div class="column right">
+                    
+                   <img src="imagenes/oficinas.jpg" width="700px">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Creado por <a href="https://www.facebook.com/jesusroberto.rosalescovarrubias">Jesus Rosales</a> | <span class="far fa-copyright"></span> 2022 Todos los derechos reservados.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
